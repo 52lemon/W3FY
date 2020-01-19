3db7c783b24f2b90804787d18879c694756c95dc
aleinwu2018 <582946945@qq.com>
2020-01-19 10:32:42 +0800

    write routes

--- routes/routes.go
+++ routes/routes.go
@@ -3,6 +3,11 @@ package routes
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"w3fy/controller/api/v1/Comment"
+	"w3fy/controller/api/v1/Likes/TagLikes"
+	"w3fy/controller/api/v1/Likes/TopicLikes"
+	"w3fy/controller/api/v1/Relation"
+	"w3fy/controller/api/v1/Topic"
 	"w3fy/controller/api/v1/User"
 	"w3fy/controller/api/v1/captcha"
 	"w3fy/middleware"
@@ -25,10 +30,66 @@ func InitRoute() *gin.Engine {
 				"msg": "pong",
 			})
 		})
-		apiv1.POST("/user/register/c1", User.RegisterByUsername)
-		apiv1.GET("/captcha/create", captcha.GetCaptcha)
-		apiv1.GET("/captcha/show/:source", captcha.ShowCaptcha)
-		apiv1.GET("/captcha/reload/:source", captcha.ReloadCaptcha)
+		//用户api
+		user := apiv1.Group("/user")
+		{
+			user.POST("/register/c1", User.RegisterByUsername)
+			user.POST("/login/c1", User.LoginByPassword)
+
+			apiv1.GET("userinfo", middleware.JWT(), User.GetUserInfo)
+			apiv1.PUT("userinfo", middleware.JWT(), User.UpdateUserInfo)
+		}
+		//验证码api
+		capt := apiv1.Group("/captcha")
+		{
+			capt.GET("/create", captcha.GetCaptcha)
+			capt.GET("/show/:source", captcha.ShowCaptcha)
+			capt.GET("/reload/:source", captcha.ReloadCaptcha)
+		}
+		//帖子接口
+		top := apiv1.Group("/topic")
+		{
+			top.POST("/", middleware.JWT(), Topic.CreateTopic)
+			top.PUT("/", middleware.JWT(), Topic.UpdateTopic)
+			top.DELETE("/", middleware.JWT(), Topic.DeleteTopic)
+			top.GET("/", middleware.JWT(), Topic.GetUserTopics)
+
+			top.GET("/normal", Topic.GetTopics)
+			top.GET("normal/:id", Topic.GetSingleTopic)
+
+			top.GET("/search/type1", Topic.TagTopics)
+			top.GET("/search/type2", Topic.TitleTopics)
+		}
+		//评论接口
+		comm := apiv1.Group("comment")
+		{
+			comm.GET("/:top_id", middleware.JWT(), Comment.GetComments)
+			comm.POST("/", middleware.JWT(), Comment.CreateComment)
+			comm.DELETE("/:id", middleware.JWT(), Comment.DeleteComment)
+		}
+		//节点收藏接口
+		tag := apiv1.Group("/taglikes")
+		{
+			tag.POST("/", middleware.JWT(), TagLikes.CreateTagLike)
+			tag.DELETE("/", middleware.JWT(), TagLikes.DeleteTagLike)
+			tag.GET("/", middleware.JWT(), TagLikes.GetTagLikes)
+		}
+		//帖子收藏接口
+		topl := apiv1.Group("/topiclikes")
+		{
+			topl.POST("/", middleware.JWT(), TopicLikes.GetTopicLikes)
+			topl.DELETE("/", middleware.JWT(), TopicLikes.DeleteTopicLike)
+			topl.GET("/", middleware.JWT(), TopicLikes.GetTopicLikes)
+		}
+		//关系接口
+		rel := apiv1.Group("/relation")
+		{
+			rel.POST("/", middleware.JWT(), Relation.CreateRelation)
+			rel.GET("/follow", middleware.JWT(), Relation.GetFollow)
+			rel.GET("/follower", middleware.JWT(), Relation.GetFollower)
+			rel.DELETE("/", middleware.JWT(), Relation.DeleteRelation)
+		}
+
 	}
 	return r
 }
